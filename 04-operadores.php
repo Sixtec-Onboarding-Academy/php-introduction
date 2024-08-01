9c7aab4a4036ed6b6433afeb9cbe841ba8e2743b
Mário Lucas <mariolucasdev@gmail.com>
2024-08-01 14:30:13 -0300

    feat: adding php operators

---
+++ 04-operadores.php
@@ -0,0 +1,44 @@
+<?php
+
+// artithmetic operators
++$x = 10;
+-$x = 10;
+
+$x + $y; // soma
+$x - $y; // subtração
+$x * $y; // multiplicação
+$x / $y; // divisão
+$x % $y; // módulo
+$x ** $y; // exponenciação
+
+// assignment operators
+$x = 10; /* atribuição */
+$x == $y; /* igual */
+$x != $y; /* diferente */
+$x <> $y; /* diferente */
+$x === $y; /* idêntico */
+$x !== $y; /* não idêntico */
+$x < $y; /* menor que */
+$x > $y; /* maior que */
+$x <= $y; /* menor ou igual */
+$x >= $y; /* maior ou igual */
+$x <=> $y; /* spaceship */
+
+/* logic operators */
+$x && $y;  /* e */
+$x || $y; /* ou */
+!$x; /* não */
+
+$x and $y; /* e */
+$x or $y; /* ou */
+
+// attribution operators
+$x = 10;
+$x += 10; // $x = $x + 10;
+$x -= 10; // $x = $x - 10;
+$x .= 10; // $x = $x . 10;
+
+$x *= 10; // $x = $x * 10;
+$x /= 10; // $x = $x / 10;
+$x %= 10; // $x = $x % 10;
+$x **= 10; // $x = $x ** 10;
